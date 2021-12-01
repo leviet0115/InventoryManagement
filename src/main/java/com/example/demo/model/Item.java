217a79fd96d6b2ebbfa39b3a7e2204f4c9ca2cb2
leviet0115 <64316728+leviet0115@users.noreply.github.com>
2021-12-01 04:44:26 +0200

    Update Item.java

--- src/main/java/com/example/demo/model/Item.java
+++ src/main/java/com/example/demo/model/Item.java
@@ -155,7 +155,7 @@ public class Item {
 	@Transient
 	public String getPhotosImagePath() {
 		if (image == null || itemId == null) return null;
-		return "/images/item/" + itemId + "/" + image;
+		return "https://raw.githubusercontent.com/leviet0115/InventoryManagement/Master/" + itemId + "/" + image;
 	}
 
 }
